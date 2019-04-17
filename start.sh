9c9ffcdc7a62b49e1399c3986673c4d210f75ed9
RainbowMiner <39437538+RainbowMiner@users.noreply.github.com>
2019-04-17 14:41:35 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+command="& {./RainbowMiner.ps1 -configfile ./Config/config.txt; exit \$lastexitcode}"
+
+while true; do
+
+  pwsh -ExecutionPolicy bypass -Command ${command}
+
+  if [ "$?" != "99" ]; then
+    break
+  fi
+
+done
